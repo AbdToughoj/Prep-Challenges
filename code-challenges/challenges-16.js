6903735c449ee11890f0fc1d668d97f91403cd22
AbdToughoj <toughoj@gmail.com>
2023-04-04 15:44:00 +0000

    secCommit

--- code-challenges/challenges-16.js
+++ code-challenges/challenges-16.js
@@ -58,6 +58,11 @@ const detectFace = (arr) => {
 
 const eveCharacter = (str) => {
     //write your code here ...
+      let newString = "";
+      for (let i = 0; i < str.length; i += 2) {
+        newString += str[i];
+      }
+      return newString;
 }
 // -------------------------------------------------------------------------------------------------------
 
